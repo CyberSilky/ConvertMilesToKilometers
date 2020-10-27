ac7dcea2c6bc9a8f2c4c5e3a3e6dfc69ea7fe8ec
James Shoemaker <james.shoemaker99@gmail.com>
2020-10-27 13:59:03 -0500

    Add project files.

---
+++ ConvertMilesToKilometers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertMilesToKilometers
+{
+    class ConvertMilesToKilometers
+    {
+        static void Main(string[] args)
+        {
+            double miles;
+            Console.Write("Enter Miles >> ");
+            miles = Convert.ToDouble(Console.ReadLine());
+
+            double convertedvalue = ConvertToKilometers(miles);
+            Console.WriteLine("{0} miles is {1} kilometers", miles, convertedvalue);
+
+        }
+
+        public static double ConvertToKilometers(double miles)
+        {
+            const double CONVERSION = 1.60934;
+            double kilometers = miles * CONVERSION;
+
+            return kilometers;
+
+        }
+    }
+}
